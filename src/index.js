e566af20a514dc6748823f6a1d4ebd0bf342c88e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-20 21:22:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,23 @@ const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
 const listEl = document.querySelector('.country-list');
 const infoEl = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  fetchCountries(name)
+    .then(countries => renderList(countries))
+    .catch(error => console.log(error));
+}
+
+function fetchCountries(name) {
+  return;
+  fetch(
+    'https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages'
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
