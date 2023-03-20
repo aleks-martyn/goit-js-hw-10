8ca4cfdf8283ece28eab4cc311737b3459ec71fb
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-20 19:27:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
