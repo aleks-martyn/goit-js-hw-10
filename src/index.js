ff317a09eb1284b4238dde52f389f7b78018881c
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-23 00:28:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -56,15 +56,16 @@ function renderCountryInfo(countries) {
     const markup = countries
       .map(country => {
         return `<div>
+        <div style="display:flex; align-items:center; gap:10px; height:50px">
         <img src="${country.flags.svg}" alt="${country.flags.alt}" width=35 height=25 />
-        <p>${country.name.official}</p>
+        <p style="font-size:24px; font-weight:bold; color:black">${country.name.official}</p>
+        </div>
         <p><b>Capital</b>: ${country.capital}</p>
         <p><b>Population</b>: ${country.population}</p>
         <p><b>Languages</b>: ${languagesArray}</p>
       </div>`;
       })
       .join('');
-    console.log(markup);
 
     infoEl.innerHTML = markup;
   }
