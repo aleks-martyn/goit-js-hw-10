ff69d9966d3c061b2c16104dbbb143176fe86afd
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-23 13:21:19 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
-export function fetchCountries(name) {
+export function fetchCountries(countryName) {
   return fetch(
-    'https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages'
+    `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
